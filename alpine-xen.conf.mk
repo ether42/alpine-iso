dc2db4c1c11a4aba07689be5a1b8d4a1cc5d7f44
Kevin Boulain <kevinboulain@gmail.com>
2016-07-30 16:32:29 +0000

    custom xen image

--- alpine-xen.conf.mk
+++ alpine-xen.conf.mk
@@ -1,7 +1,5 @@
 ALPINE_NAME     := alpine-xen
 MODLOOP_EXTRA   :=
 KERNEL_FLAVOR   := grsec
-# Example of serial console boot and Dom0 memory limit
-#XEN_PARAMS	:= com1=115200,8n1 console=com1 dom0_mem=512M
-#BOOT_OPTS	:= console=hvc0
+XEN_PARAMS	:= dom0_mem=512M dom0_max_vcpus=1 dom0_vcpus_pin
 BOOT_OPTS	:= nomodeset
